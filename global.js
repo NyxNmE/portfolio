57d21fa6b0a9a010c3f6b1f44a0f977f0d90ea47
NyxNmE <115585906+NyxNmE@users.noreply.github.com>
2025-01-26 09:45:02 -0800

    s

--- global.js
+++ global.js
@@ -1,5 +1,40 @@
-console.log('IT’S ALIVE!');
+console.log("IT'S ALIVE!");
 
-function $$(selector, context = document) {
+export function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
+}
+
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
+
+const pages = [
+  { url: "",            title: "Home" },
+  { url: "projects/",   title: "Projects" },
+  { url: "contact/",    title: "Contact" },
+  { url: "resume/",     title: "Resume" },
+  { url: "https://github.com/NyxNmE", title: "GitHub" }
+];
+
+const nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let a = document.createElement("a");
+
+  let finalURL = p.url;
+  if (!ARE_WE_HOME && !finalURL.startsWith("http")) {
+    finalURL = "../" + finalURL;
+  }
+
+  a.href = finalURL;
+  a.textContent = p.title;
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add("current");
+  }
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
 }
