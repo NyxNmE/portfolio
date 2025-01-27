9ac57850ec7542590a25e43bc396db6e064bd53a
NyxNmE <115585906+NyxNmE@users.noreply.github.com>
2025-01-26 16:15:32 -0800

    s

--- global.js
+++ global.js
@@ -4,42 +4,37 @@ export function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a");
-const currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
-if (currentLink) {
-  currentLink.classList.add("current");
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
+
+const pages = [
+  { url: "",            title: "Home" },
+  { url: "projects/",   title: "Projects" },
+  { url: "contact/",    title: "Contact" },
+  { url: "resume/",     title: "Resume" },
+  { url: "https://github.com/NyxNmE", title: "GitHub" }
+];
+
+const nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let a = document.createElement("a");
+
+  let finalURL = p.url;
+  if (!ARE_WE_HOME && !finalURL.startsWith("http")) {
+    finalURL = "../" + finalURL;
+  }
+
+  a.href = finalURL;
+  a.textContent = p.title;
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add("current");
+  }
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
 }
-
-document.body.insertAdjacentHTML(
-  "afterbegin",
-  `
-    <label class="color-scheme">
-      Theme:
-      <select id="theme-select">
-        <option value="light dark">Automatic</option>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-      </select>
-    </label>
-  `
-);
-
-const select = document.querySelector("#theme-select");
-
-function setColorScheme(mode) {
-  document.documentElement.style.setProperty("color-scheme", mode);
-  localStorage.colorScheme = mode;
-}
-
-if (localStorage.colorScheme) {
-  setColorScheme(localStorage.colorScheme);
-  select.value = localStorage.colorScheme;
-}
-
-select.addEventListener("input", (event) => {
-  const mode = event.target.value;  
-  setColorScheme(mode);
-  console.log("color scheme changed to", mode);
-});
