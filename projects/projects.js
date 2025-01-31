69b4754d53a234182ebdb34c2f06d8c954d3fad7
NyxNmE <115585906+NyxNmE@users.noreply.github.com>
2025-01-30 18:51:51 -0800

    3.2 attempt part 2

--- projects/projects.js
+++ projects/projects.js
@@ -3,16 +3,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 (async function () {
   const projects = await fetchJSON('../lib/projects.json');
 
-  if (!projects || projects.length === 0) {
-    console.error("No projects found.");
-    return;
-  }
-
   const projectsContainer = document.querySelector('.projects');
 
-  if (projectsContainer) {
-    renderProjects(projects, projectsContainer, 'h2');
-  } else {
-    console.error("Projects container not found.");
-  }
+  renderProjects(projects, projectsContainer, 'h2');
 })();
