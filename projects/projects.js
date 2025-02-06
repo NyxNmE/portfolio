90c16524b2a7153c9705f36bc0de708f4bd0b645
NyxNmE <115585906+NyxNmE@users.noreply.github.com>
2025-02-06 06:36:09 -0800

    1.2 attempt

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 (async function () {
   const projects = await fetchJSON('../lib/projects.json');
@@ -13,4 +14,19 @@ import { fetchJSON, renderProjects } from '../global.js';
   } else {
     console.error("Projects container not found.");
   }
+
+  const svg = d3.select('#projects-pie-plot');
+
+  let arcGenerator = d3.arc()
+    .innerRadius(0)
+    .outerRadius(50);
+
+  let arc = arcGenerator({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+  });
+
+  svg.append('path')
+    .attr('d', arc)
+    .attr('fill', 'red');
 })();
