d2f78ca1887a0f9fcf6fed3f290902a2991f744f
NyxNmE <115585906+NyxNmE@users.noreply.github.com>
2025-02-13 14:38:51 -0800

    lab 6 step 1.1

--- meta/main.js
+++ meta/main.js
@@ -1,18 +1,10 @@
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
-
 let data = [];
 
 async function loadData() {
   data = await d3.csv('loc.csv');
-  console.log("Loaded LOC data:", data);
-
-  let totalLines = 0;
-  data.forEach(row => {
-    totalLines += +row.lines;
-  });
-
-  const statsDiv = document.getElementById('stats');
-  statsDiv.textContent = `Total lines of code: ${totalLines}`;
+  console.log(data);
 }
 
-document.addEventListener('DOMContentLoaded', loadData);
+document.addEventListener('DOMContentLoaded', async () => {
+  await loadData();
+});
